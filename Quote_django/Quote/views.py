829d6fb1007cac3d6be18baf52b8eeb39a7eead9
Ar Monarch <praffulthapa12@gmail.com>
2023-07-25 10:40:04 +0545

    Implemented Dynamic Routing For Categories in Quote

--- Quote_django/Quote/views.py
+++ Quote_django/Quote/views.py
@@ -2,14 +2,25 @@ from django.shortcuts import render
 import requests
 
 # Create your views here.
-def Quote(request):
-    category = "inspirational"
+def Any_Quote(request):
+    category = "" #From Any Category
     api_url = f"https://api.api-ninjas.com/v1/quotes?category={category}"
     try:
         response = requests.get (api_url, headers={'X-Api-Key': 'c9Ys6JnpsqXdmr7ARqPCFw==YXL1AJg4LStpKrMQ'})
         response.raise_for_status()
         Data = response.json()
         return render(request, 'Quote/Quote.html', {'Quote':Data})    
+    except requests.exceptions.RequestException as e:
+        print("Error fetching data from api:", e)
+        return render(request, "")
+    
+def Quote(request, category):
+    api_url = f"https://api.api-ninjas.com/v1/quotes?category={category}"
+    try:
+        response = requests.get(api_url, headers={'X-Api-Key': 'c9Ys6JnpsqXdmr7ARqPCFw==YXL1AJg4LStpKrMQ'})
+        response.raise_for_status()
+        Data = response.json()
+        return render(request, "Quote/Quote.html", {'Quote': Data})
     except requests.exceptions.RequestException as e:
         print("Error fetching data from api:", e)
         return render(request, "")
