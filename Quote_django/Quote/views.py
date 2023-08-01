251f007b08768f47482e2da1571851f9e81da0f1
Ar Monarch <praffulthapa12@gmail.com>
2023-08-01 10:22:20 +0545

    configured Enviromnent variables

--- Quote_django/Quote/views.py
+++ Quote_django/Quote/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
+import os
+import environ
 import requests
+from django.shortcuts import render
+
 from .models import Category as CategoryModel
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env(os.path.join("../", ".env"))
 
 # Create your views here.
 def Quote(request):
     category = request.GET.get("category", "")
+    api_key = env('Quote_Api_key')
     api_url = f"https://api.api-ninjas.com/v1/quotes?category={category}"
     try:
-        response = requests.get(api_url, headers={'X-Api-Key': 'c9Ys6JnpsqXdmr7ARqPCFw==YXL1AJg4LStpKrMQ'})
+        response = requests.get(api_url, headers={'X-Api-Key': api_key})
         response.raise_for_status()
         Data = response.json()
         return render(request, "Quote/Quote.html", {'Quote': Data})
