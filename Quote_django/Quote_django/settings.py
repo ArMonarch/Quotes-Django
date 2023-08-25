a12bb0035a78991b005bb4d3392347e09d4fb847
Ar Monarch <praffulthapa12@gmail.com>
2023-08-25 13:00:49 +0545

    added 127.0.0.1 to allowded host

--- Quote_django/Quote_django/settings.py
+++ Quote_django/Quote_django/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["140.238.228.249","localhost"]
+ALLOWED_HOSTS = ["140.238.228.249","localhost",'127.0.0.1']
 
 
 # Application definition
